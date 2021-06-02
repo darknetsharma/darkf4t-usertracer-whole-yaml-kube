6eba667dedbf1243feee63510b8b9220f04d7421
darknetsharma <sawan.sharma000666@gmail.com>
2021-06-02 21:15:37 +0000

    v1

---
+++ darkf4t-usertracer.sh
@@ -0,0 +1,2 @@
+kubectl create -f MariaDB/darkf4t-mariadb.yaml
+kubectl create -f PhpMyAdmin/darkf4t-phpmyadmin.yaml
---
+++ delete-darkf4t-usertracer.sh
@@ -0,0 +1,2 @@
+kubectl delete deploy darkf4t-mariadb-deploy darkf4t-phpmyadmin-deploy
+kubectl delete svc darkf4t-mariadb-svc darkf4t-phpmyadmin-svc
